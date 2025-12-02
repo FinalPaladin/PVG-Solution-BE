eb8ea96661486f8769e8a5b0fd365b8931ae6132
Tran Canh Vinh <tcvinh.0702@gmail.com>
2025-12-02 14:12:18 +0700

    update start up config swagger

--- PVG.Web/Startup.cs
+++ PVG.Web/Startup.cs
@@ -138,7 +138,11 @@ namespace PVG.Web
             //app.UseCors(_policyName);
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "PVG Services v1"); c.RoutePrefix = "swagger"; });
+            app.UseSwaggerUI(c => 
+            { 
+                c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "PVG Services v1"); 
+                c.RoutePrefix = "api/swagger"; 
+            });
 
             // Add this block to perform migration using the application's service provider
             using (var scope = app.ApplicationServices.CreateScope())
